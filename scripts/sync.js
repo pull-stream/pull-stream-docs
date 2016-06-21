b31ae7e10fdf5be4e9ec8137a4df4b779c1dba6b
Michael Williams <michael.williams@enspiral.com>
2016-06-21 17:50:04 +1200

    use GH_TOKEN if in process.env, for travis deploy

--- scripts/sync.js
+++ scripts/sync.js
@@ -10,17 +10,24 @@ module.exports = syncModules
 function syncModules (modules, done) {
   const moduleList = util.toModuleList(modules)
 
-  ghAuth({
-    configName: 'ecosystem-docs',
-    userAgent: 'ecosystem-docs',
-    scopes: ['user']
-  }, (err, auth) => {
-    if (err) { return done(err) }
-
+  function sync (token) {
     ecosystem.sync(moduleList, {
       data: config.data,
-      token: auth.token
+      token: token
     }, done)
-  })
+  }
+
+  if (process.env.GH_TOKEN) {
+    sync(process.env.GH_TOKEN)
+  } else {
+    ghAuth({
+      configName: 'ecosystem-docs',
+      userAgent: 'ecosystem-docs',
+      scopes: ['user']
+    }, (err, auth) => {
+      if (err) { return done(err) }
+      sync(auth.token)
+    })
+  }
 }
 
